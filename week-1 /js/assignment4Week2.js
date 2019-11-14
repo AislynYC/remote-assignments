86652b07a19210c255829bb766f6379a641f1bfc
AislynYC <chuang.yuchun@gmail.com>
2019-11-14 16:14:22 +0800

    complete assignment 4 request 2

--- week-1 /js/assignment4Week2.js
+++ week-1 /js/assignment4Week2.js
@@ -12,3 +12,15 @@ mainBanner.addEventListener('click', changeMsg);
 //Request 2: Click to Show/Close Menu.
 
 var menuBtn = document.getElementById('main-menu-btn');
+var navCard = document.getElementById('nav-card');
+var menuCloseBtn = document.getElementById('menu-close-btn');
+
+function openMenuCard() {
+    navCard.style.display = 'block';
+}
+
+function closeMenuCard() {
+    navCard.style.display = 'none';
+}
+menuBtn.addEventListener('click', openMenuCard);
+menuCloseBtn.addEventListener('click', closeMenuCard);
