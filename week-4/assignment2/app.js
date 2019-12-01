a873d0db96355c9a89e38a700b9ed160b8c155c5
AislynYC <chuang.yuchun@gmail.com>
2019-12-01 17:54:24 +0800

    complete assignment 2

--- week-4/assignment2/app.js
+++ week-4/assignment2/app.js
@@ -1,8 +1,8 @@
 function ajax(src, callback) {
   // your code here
   const xhr = new XMLHttpRequest();
+  xhr.open('GET', src);
   xhr.onreadystatechange = function() {
-    console.log(xhr.responseText);
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
         callback(xhr.responseText);
@@ -10,16 +10,26 @@ function ajax(src, callback) {
         alert(`[${xhr.status}] ${xhr.statusText}`);
       }
     }
-
-    xhr.open('GET', src);
-    xhr.send();
   };
+  xhr.send();
 }
 function render(data) {
   // your code here.
   //  document.createElement()  and appendChild()  methods are preferred.
-  const dataDiv = document.createElement('div');
-  dataDiv.appendChild(JSON.stringify(data));
+
+  const body = document.getElementsByTagName('body')[0];
+  const dataObj = JSON.parse(data);
+
+  Object.values(dataObj).forEach(function(item) {
+    let dataDiv = document.createElement('div');
+    let template = `<div class="card">
+                                <h3 class="card-title">${item.name}</h3>
+                                <p class="card-text">${item.description}</p>
+                                <p class="card-text">售價:${item.price}元</p>
+                        </div>`;
+    dataDiv.innerHTML = template;
+    body.appendChild(dataDiv);
+  });
 }
 
 ajax(
